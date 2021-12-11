0939acc40b74ee50532d0014b5285e480f042fef
unknown <mose23@naver.com>
2021-12-11 23:26:40 +0900

    Create Submit Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import FeedbackItem from './components/FeedbackItem'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import FeedbackData from './data/FeedbackData'
 
 function App() {
+  const [feedBack, setFeedBack] = useState(FeedbackData)
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?'))
+      setFeedBack(feedBack.filter((item) => item.id !== id))
+  }
+
   return (
     <>
       <Header />
       <div className='container'>
-        <h1>My App</h1>
+        <FeedbackForm />
+        <FeedbackStats feedBack={feedBack} />
+        <FeedbackList feedback={feedBack} handleDelete={deleteFeedback} />
       </div>
     </>
   )
---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+
+function Card({ children, reverse }) {
+  // return <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+
+  // console.log(children)
+  // Children 은 html 태그를 불러온다.
+  return (
+    <div
+      className='card'
+      style={{
+        backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+        color: reverse ? '#fff' : '#000',
+      }}
+    >
+      {children}
+    </div>
+  )
+}
+
+Card.defaultProps = {
+  reverse: false,
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  reverse: PropTypes.bool,
+}
+
+export default Card
